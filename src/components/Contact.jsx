1516c5108c40ea2bae64229bd81d4fdd238e787c
timm6430 <timm6430@pacificu.edu>
2024-02-21 09:30:26 -0800

    Finished contact email part

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#186EF8] flex justify-center items-center p-4'>
-        <form method='POST' action="" className='flex flex-col max-w-[600px] w-full'>
+        <form method='POST' action="https://getform.io/f/DbDGGBal" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#F8A218] text-gray-300'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - vutukuri.kumar192st.niituniversity.in</p>
